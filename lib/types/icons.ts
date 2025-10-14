20de4a01216548c00ad9c177a8c129cb8a6f83d5
Simon Nyström <simons.nystrom@gmail.com>
2025-10-14 13:46:59 +0200

    swtiched to sprite based icon handling with automatic type safety

---
+++ lib/types/icons.ts
@@ -0,0 +1,27 @@
+// Auto-generated from sprite.svg
+export const ICON_NAMES = [
+  'arrow',
+  'back',
+  'calendar',
+  'chat',
+  'check',
+  'component',
+  'copy',
+  'design',
+  'document',
+  'external',
+  'gallery',
+  'github',
+  'heart',
+  'hourglass',
+  'info',
+  'instagram',
+  'link',
+  'linkedin',
+  'moon',
+  'rss',
+  'sun',
+  'warning'
+] as const;
+
+export type IconName = typeof ICON_NAMES[number];
