546f5c3ce8c4bdb08189643fb0d939c44e065a79
Simon Nyström <simons.nystrom@gmail.com>
2025-10-18 01:35:23 +0200

    reusable button component

--- components/ui/ThemeSwitcher.tsx
+++ components/ui/ThemeSwitcher.tsx
@@ -3,18 +3,21 @@
 import { cn } from '@/lib/utils';
 import { useTheme } from 'next-themes';
 import { Icon } from './Icon';
+import { Button } from './Button';
 
 export default function ThemeSwitcher({ className }: { className?: string }) {
 	const { setTheme, resolvedTheme } = useTheme();
 
 	return (
-		<button
+		<Button
+			variant="ghost"
+			size="icon"
 			aria-label={`Change to ${resolvedTheme === 'dark' ? 'light' : 'dark'} mode`}
 			onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-			className={cn('cursor-pointer', className)}
+			className={cn('relative cursor-pointer', className)}
 		>
 			<Icon name="sun" className="absolute hidden dark:block" />
 			<Icon name="moon" className="absolute dark:hidden" />
-		</button>
+		</Button>
 	);
 }
