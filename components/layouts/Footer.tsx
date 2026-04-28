ad5293ea5d7cd53f59ae8b16c4eb6483573205a3
Simon Nyström <simons.nystrom@gmail.com>
2026-04-28 21:16:19 +0200

    changed footer to display version and commit

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -2,6 +2,7 @@ import { getLatestCommit } from '@/lib/github';
 import CustomLink from '../blog/Link';
 import { Icon } from '../ui/Icon';
 import { cn } from '@/lib/utils';
+import { APP_VERSION } from '@/data/constants';
 
 export default async function Footer() {
 	const commitInfo = await getLatestCommit();
@@ -9,23 +10,23 @@ export default async function Footer() {
 	return (
 		<footer
 			className={cn(
-				'max-w-3xl w-full mx-auto mt-auto flex flex-col justify-between gap-y-4 p-6',
+				'max-w-3xl w-full mx-auto mt-auto flex flex-col justify-between gap-y-1 p-6',
 				'border-t border-dashed text-foreground-tertiary text-xs',
 				'sm:flex-row sm:items-center sm:border-t-0 sm:pb-12',
 			)}
 		>
+			<div>
+				<span className="font-[450]">v{APP_VERSION} </span>
+			</div>
 			<div className="flex items-center gap-1">
-				<Icon name="code" className="size-4" />
-				Last commit:{' '}
-				{commitInfo?.url ? (
+				{commitInfo?.url && (
 					<CustomLink
+						icon="commit"
 						href={commitInfo.url}
 						className="pb-1 font-code text-foreground-secondary hover:text-foreground before:h-px"
 					>
 						{commitInfo.sha?.slice(0, 7)}
 					</CustomLink>
-				) : (
-					<span className="pb-1 font-code">N/A</span>
 				)}
 			</div>
 		</footer>
